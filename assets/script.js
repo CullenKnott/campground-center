4f10697e86bc702e5d0584a73bc613309d7c4193
RubenRuiz411 <rubenmruiz.411@gmail.com>
2023-06-01 22:26:14 -0700

    spelling error fixed

--- assets/script.js
+++ assets/script.js
@@ -102,7 +102,7 @@ function apiCall() {
 function weathernametemp(data) {
   var weatherCityName = data.name;
   var cityNameelement = document.querySelector(".weatherCardCityName")
-  cityNameelement.textContent = "10 Campgrounds locations in the city of "+ weatherCityName;
+  cityNameelement.textContent = "10 Campground locations in the city of "+ weatherCityName;
   var actualTemp = data.main.temp;
   var tempElement = document.querySelector(".weatherCardTemp")
   tempElement.textContent= "Temperature in " + weatherCityName + " is currently " + actualTemp + "°F ";  
